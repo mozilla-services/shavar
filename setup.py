7fe71a5be04a353621ef7ac19cb64bdc6889cb6b
Ryan Tilder <ryan@tilder.org>
2015-07-01 11:43:25 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'requirements.txt')) as f:
 
 
 setup(name='shavar',
-      version='0.6.2',
+      version='0.6.3',
       description='shavar',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
