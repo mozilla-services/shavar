a8bb584059bca3234f20ec515e5766877eedc887
Chris Kolosiwsky <ckolos@gmail.com>
2016-08-26 15:43:50 -0500

    add an exec to the start up script pass START arg to circle test missing space; added add a push for testing_.* branches add code required to install the newrelic agent in the container if desired add logic to start via the newrelic wrapper if -e ENABLE_NEWRELIC is set on the docker run command line circleci's docker doesn't support the ARG spec. Lame. ignore newrelic.ini if it exists require newrelic unstage change to requirements.txt Dockerfile changes - set a sane default for NEWRELIC_LIC_KEY - Don't generate newrelic.ini on container build, generate at runtime in response to ENV vars set on command line instead startup.sh changes if ENABLE_NEWRELIC is set and the config file doesn't exist. Generate one and set the appropriate app name. The newrelic-admin script is brain dead and doesn't accept any options for setting this. Check to make sure RUN_NEWRELIC is actually set to TRUE. Quote ENV setting too s/;/:/ add a DEPLOY option to startup script if newrelic is enabled

--- startup.sh
+++ startup.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
-test -n "$1" && ARG=$(echo $1 | tr [a-z] [A-Z])
-if [[ "$ARG" == "START" ]]; then
-  test -x /usr/local/bin/uwsgi && \
-      /usr/local/bin/uwsgi --ini-paste shavar.ini --paste-logger --uid 10001 --gid 10001
+
+
+function start {
+  if [ "$RUN_NEWRELIC" == "TRUE" ]; then
+    NEW_RELIC_CONFIG_FILE="./newrelic.ini" newrelic-admin run-program /usr/local/bin/uwsgi --ini-paste shavar.ini --paste-logger --uid 10001 --gid 10001
+  else
+     exec /usr/local/bin/uwsgi --ini-paste shavar.ini --paste-logger --uid 10001 --gid 10001
+  fi
+}
+
+
+function deploy {
+  if [ "$RUN_NEWRELIC" == "TRUE" ]; then
+    HASH=$(cat version.json | cut -d":" -f2|cut -d"," -f1 | tr -d "\"")
+    REPO=$(cat version.json | cut -d"\"" -f12)
+    newrelic-admin record-deploy ./newrelic.ini ${HASH:7} ${REPO} svcops
+  else
+    /bin/false
+  fi
+}
+
+
+if [ -n "$1" ]; then
+  COMMAND=$(echo $1 | tr [a-z] [A-Z])
 else
-  echo "Invalid option specified: ARG=$ARG"
-  exit
-fi  
+  exit 1
+fi
+
+if env | grep -q "ENABLE_NEWRELIC"; then
+  declare -x "RUN_NEWRELIC=TRUE"
+  if [ -e newrelic.ini ]; then
+    /bin/true
+  else
+    newrelic-admin generate-config ${NEWRELIC_LIC_KEY} newrelic.ini
+    sed -i -e 's@app_name = Python Application@app_name = Shavar@' newrelic.ini
+  fi
+fi
+
+if [ -x /usr/local/bin/uwsgi ]; then
+  case "$COMMAND" in
+    START)
+      start
+      exit 0
+      ;;
+    DEPLOY)
+      deploy
+      exit 0
+      ;;
+    *)
+      echo "Invalid option specified: ARG=$COMMAND"
+      exit 1
+      ;;
+  esac
+else
+  echo "uwsgi is not installed"
+  exit 1
+fi
